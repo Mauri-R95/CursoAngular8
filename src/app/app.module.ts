8f032e65e238597e507f7135c35e811e765e6be6
Mauricio Javier Ruiz Torres <mauricioruiz@konexa.cl>
2020-12-29 19:04:57 -0300

    correos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CorreoComponent } from './Components/correo/correo.component';
+//fichero que esta toda la informacion de las importaciones
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CorreoComponent
   ],
   imports: [
     BrowserModule,
